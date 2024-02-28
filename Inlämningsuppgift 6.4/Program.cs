05e11d17bfacfe91723bf178d53eeae2def4c820
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2024-02-28 10:43:44 +0100

    Add project files.

---
+++ Inlämningsuppgift 6.4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace uppgift_6._4
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Potenser(2, 4));
+        }
+        static int Potenser(int bas, int exp)
+        {
+            int produkt = bas;
+            for (int i = 1; i < exp; i++)
+            {
+                produkt *= bas;
+            }
+            return produkt;
+        }
+    }
+}
